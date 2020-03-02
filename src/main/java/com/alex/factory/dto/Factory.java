87bfadd352f6589f914343113dd98da2af08731d
Spawn <alkspawn@gmail.com>
2020-03-02 06:17:20 +0300

    feat: update

--- src/main/java/com/alex/factory/dto/Factory.java
+++ src/main/java/com/alex/factory/dto/Factory.java
@@ -2,9 +2,11 @@ package com.alex.factory.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class Factory extends AbstractAccount{
-
+    @NotNull
     private String position;
 
 }
--- src/main/java/com/alex/factory/dto/LoginForm.java
+++ src/main/java/com/alex/factory/dto/LoginForm.java
@@ -2,8 +2,12 @@ package com.alex.factory.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class LoginForm {
+    @NotNull
     private String email;
+    @NotNull
     private String password;
 }
